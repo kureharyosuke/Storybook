2c2a99b117007fce75d2c2292b957f452a3f47b6
kureharyosuke <ohhayo.com@gmail.com>
2021-04-23 12:37:12 +0900

    add: Hello text

---
+++ src/Hello/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hello = ({name, big}) => {
+    if (big) {
+        return <h1>안녕하세요. {name}</h1>
+    }
+    return <p>안녕하세요. {name}!</p>
+    // big ? <h1>안녕하세요. {name}</h1> : <p>안녕하세요. {name}</p>
+}
+
+export default Hello
